46cc333d2afff31356d1c62bd10c5a7ee9f1dbe3
gongkangjia <gongkangjia@gmail.com>
2018-07-27 16:34:20 +0800

    kk

---
+++ cov_not_nan_to_1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randn(10,6))
+
+# Make a few areas have NaN values
+
+df.iloc[1:3,1] = np.nan
+df.iloc[5,3] = np.nan
+df.iloc[7:9,5] = np.nan
